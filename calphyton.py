2125db7f5f4506425a7286e80acf5e4623ff11cd
João Crispim Neto <32086073+crispimneto@users.noreply.github.com>
2018-09-17 20:12:45 -0300

    Add files via upload

--- calphyton.py
+++ calphyton.py
@@ -3,6 +3,8 @@
 # João Crispim Neto - 17 de setembro de 2018
 
 # Definição das funções básicas para os cálculos.
+import math
+
 def soma(nr1,nr2):
     return(nr1+nr2)
 
@@ -20,7 +22,7 @@ def menu():
     print ('='* 44)
     print ('| 1 = SOMA' + '                  ' + '6 = NOVA OPÇÃO |')
     print ('| 2 = SUBTRAÇÃO' + '             ' + '7 = NOVA OPÇÃO |')
-    print ('| 3 = DIVISÃO'  +'               ' + '8 = NOVA OPÇÃO |')
+    print ('| 3 = MULTIPLICAÇÃO'  +'         ' + '8 = NOVA OPÇÃO |')
     print ('| 4 = DIVISÃO'  +'               ' + '9 = NOVA OPÇÃO |')
     print ('| 5 = RAIZ QUADRADA' + '         ' + '0 = SAIR       |')
     print ('=' * 44)
@@ -34,27 +36,43 @@ menu = int(input('Digite uma Opção: ZERO para Sair: '))
 while menu != 0:
     
     if menu == 1:
-        vlr1 = int(input('Digite o Primeivo Valor: '))
+        vlr1 = int(input('Digite o Primeiro Valor: '))
         vlr2 = int(input('Digite o Segundo Valor: '))
         print('A Soma é = ' , soma(vlr1,vlr2))
         menu = int(input('Digite uma Opção: ZERO para Sair: '))
     elif menu == 2:
-        vlr1 = int(input('Digite o Primeivo Valor: '))
+        vlr1 = int(input('Digite o Primeiro Valor: '))
         vlr2 = int(input('Digite o Segundo Valor: '))
         print('A Subtração é = ', sub(vlr1,vlr2))
         menu = int(input('Digite uma Opção: ZERO para Sair: '))
     elif menu == 3:
-        vlr1 = int(input('Digite o Primeivo Valor: '))
+        vlr1 = int(input('Digite o Primeiro Valor: '))
         vlr2 = int(input('Digite o Segundo Valor: '))
         print('A Multiplicação é = ', mult(vlr1,vlr2))
         menu = int(input('Digite uma Opção: ZERO para Sair: '))
     elif menu == 4:
-        vlr1 = int(input('Digite o Primeivo Valor: '))
+        vlr1 = int(input('Digite o Primeiro Valor: '))
         vlr2 = int(input('Digite o Segundo Valor: '))
         while vlr2 == 0:
             print('O Valor não pode ser ZERO - Redigite: ')
             vlr2 = int(input('Digite o Segundo Valor: '))
         print(f'A Divisão é =  {div(vlr1,vlr2):.2f}')
+        menu = int(input('Digite uma Opção: ZERO para Sair: '))
+    elif menu == 5:
+        vlr1 = int(input('Digite o Valor para Calculo da Raiz Quadrada: '))
+        print (f'A raiz de {vlr1} é igual a {(math.sqrt(vlr1)):.2f}')
+        menu = int(input('Digite uma Opção: ZERO para Sair: '))
+    elif menu == 6:
+        print('Opção ainda nao Implementada')
+        menu = int(input('Digite uma Opção: ZERO para Sair: '))
+    elif menu == 7:
+        print('Opção ainda nao Implementada')
+        menu = int(input('Digite uma Opção: ZERO para Sair: '))
+    elif menu == 8:
+        print('Opção ainda nao Implementada')
+        menu = int(input('Digite uma Opção: ZERO para Sair: '))
+    elif menu == 9:
+        print('Opção ainda nao Implementada')
         menu = int(input('Digite uma Opção: ZERO para Sair: '))
     else:
         menu = int(input('Digite uma Opção: ZERO para Sair: '))
